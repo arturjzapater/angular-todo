4643df5f082f9fc3a46fa00d9b6aecbd2b16ca53
arturjzapater <zapater.artur@gmail.com>
2020-10-16 14:05:03 +0200

    added basic todo functionality

---
+++ src/app/todo-list/todo-list.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+
+import { TodoService } from '../todo.service';
+import type { Todo } from '../todo';
+
+@Component({
+  selector: 'app-todo-list',
+  templateUrl: './todo-list.component.html',
+  styleUrls: ['./todo-list.component.css']
+})
+export class TodoListComponent implements OnInit {
+  todos: Todo[]
+
+  constructor(private todoService: TodoService) { }
+
+  ngOnInit(): void {
+    this.getTodos()
+  }
+
+  add(name: string): void {
+    this.todoService.add(name)
+      .subscribe(list => this.todos = list)
+  }
+
+  getTodos(): void {
+    this.todoService.list()
+      .subscribe(list => this.todos = list)
+  }
+}
